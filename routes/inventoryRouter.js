bb892a7104406de487c3f063efbf694a342aa3c6
strom314 <jakub.gros777@gmail.com>
2026-01-08 18:40:22 +0100

    edit the database and create mockup routes

---
+++ routes/inventoryRouter.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router();
+
+router.get("/",)
+router.get("/games")
+router.get("/games/:gameId")
+router.get("/categories")
+router.get("/new")
+router.post("/new")
+
+
