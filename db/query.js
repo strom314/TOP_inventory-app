bafd60e720e386b6c9feac6192e429125ba28e0f
strom314 <jakub.gros777@gmail.com>
2026-01-09 09:27:52 +0100

    write create queries in query.js

--- db/query.js
+++ db/query.js
@@ -1,5 +1,7 @@
 const pool = require("./pool");
 
+//GET QUERIES
+
 async function getAllGames() {
   const games = await pool.query("SELECT * FROM inventory_games");
   return games.rows;
@@ -22,3 +24,21 @@ async function getCategory(id) {
   return category.rows;
 }
 
+//CREATE QUERIES
+
+async function createGame(title, price, developer_id, category_id) {
+  await pool.query(
+    `INSERT INTO inventory_games(title, price, developer_id, category_id) VALUES ($1, $2, $3, $4)`,
+    [title, price, developer_id, category_id]
+  );
+}
+async function createCategory(name) {
+  await pool.query(`INSERT INTO inventory_categories(name) VALUES($1)`, [name]);
+}
+
+module.exports = {
+  getAllGames,
+  getAllCategories,
+  getGame,
+  getCategory,
+};
